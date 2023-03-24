d3b97a406814cfb3c34c4d4aacb324ae9361fdcc
Hunter Rafuse <hunter_r_mazza@protonmail.com>
2023-03-24 16:52:51 +0200

    Add test

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,25 @@
-fn main() {
+use std::arch::asm;
 
-    println!("Hello, world!");
+
+fn main() {
+    print!("Bootloader\n");
+
+    // let mut __bootrom_start__: u32 = 0;
+    // let mut __approm_start__: u32 = 0;
+    // let mut _app_code:u32 = __approm_start__;
+    // let c: u8 = 10;
+    // let ax = u16::from(c) | 0x0e00;
+
+    unsafe {
+
+        asm!(
+            "push bx",
+            "mov bx, 0",
+            "int 0x10",
+            "pop bx",
+            in("ax") ax
+        );
+        
+    }
 
 }
